75543c5cb8198640e611d0509527faca86331ce2
David <xdfnet@gmail.com>
2026-04-03 18:53:48 +0800

    1

--- iClashSource/StatusBarController.swift
+++ iClashSource/StatusBarController.swift
@@ -23,7 +23,7 @@ final class StatusBarController: NSObject {
     func updateStatusIcon(isRunning: Bool) {
         DispatchQueue.main.async { [weak self] in
             guard let button = self?.statusBarItem?.button else { return }
-            let symbolName = isRunning ? "circle.fill" : "circle"
+            let symbolName = isRunning ? "chart.bar.fill" : "chart.bar"
             if let image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "iClash") {
                 image.isTemplate = true
                 button.image = image
